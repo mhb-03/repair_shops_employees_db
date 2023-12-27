467419fb68dd3b92f5866d243e5416903e3cf806
MhdHklBhq <127035513+mhb-03@users.noreply.github.com>
2023-12-27 14:15:47 +0700

    Update add_city.php

--- City/add_city.php
+++ City/add_city.php
@@ -1,48 +1,52 @@
 <?php
-// Include file koneksi dan fungsi query
-include 'config.php';
-$conn = mysqli_connect('localhost', 'root', '', 'repair_shops_employees');
-// Cek apakah form telah di-submit
-if (isset($_POST['submit'])) {
-    // Panggil fungsi tambah
-    if (add_city($_POST) > 0) {
+    // Include file koneksi dan fungsi query
+    include 'config.php';
+    $conn = mysqli_connect('localhost', 'root', '', 'repair_shops_employees');
+    // Cek apakah form telah di-submit
+    if (isset($_POST['submit']))
+    {
+        // Panggil fungsi tambah
+        if (add_city($_POST) > 0) {
         echo "<script>
 			alert('Data berhasil ditambahkan!');
 			document.location.href='city.php';
 			</script>";
-    } else {
+        } else {
         echo "<script>
 			alert('Data gagal ditambahkan!');
 			document.location.href='city.php';
 			</script>";
+        }
     }
-}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add city</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Add city</title>
+    </head>
 
-<h2>Add New city</h2>
+    <body>
 
-<form action="add_city.php" method="post">
-<label for="id">id:</label>
-    <input type="number" name="id" required>
-    <label for="postal_code">postal_code:</label>
-    <input type="text" name="postal_code" required>
-    <label for="city_name">city_name:</label>
-    <input type="text" name="city_name" required>
-    <br>
-    <input type="submit" name= "submit" >
-</form>
+        <h2>Add New city</h2>
 
-<br>
+        <form action="add_city.php" method="post">
+            <label for="id">id:</label>
+                <input type="number" name="id" required>
+            <label for="postal_code">postal_code:</label>
+                <input type="text" name="postal_code" required>
+            <label for="city_name">city_name:</label>
+                <input type="text" name="city_name" required>
+            <br>
+                <input type="submit" name= "submit" >
+        </form>
 
-<a href="city.php">Back to city List</a>
-</body>
+            <br>
+
+        <a href="city.php">Back to city List</a>
+
+    </body>
+
 </html>
